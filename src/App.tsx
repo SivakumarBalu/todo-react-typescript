285b849236bec298a7881fb81c9ee1e5b24f5c0e
SivakumarBalu <postboxno100@gmail.com>
2022-01-03 15:41:20 -0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,62 @@
+import { ChangeEvent, FC, useState } from "react";
+import "./styles.css";
+import { ITask } from "./interface";
+import TodoTask from "./TodoTask";
+
+const App: FC = () => {
+  const [task, setTask] = useState<string>("");
+  const [deadLine, setDeadLine] = useState<number>(0);
+  const [todoList, setTodoList] = useState<ITask[]>([]);
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
+    if (event.target.name === "task") {
+      setTask(event.target.value);
+    } else {
+      setDeadLine(Number(event.target.value));
+    }
+  };
+  const addTask = (): void => {
+    const newTask = {
+      taskName: task,
+      deadline: deadLine
+    };
+    setTodoList([...todoList, newTask]);
+  };
+  const completeTask = (taskNameToDelete: string): void => {
+    setTodoList(
+      todoList.filter((task) => {
+        return task.taskName !== taskNameToDelete;
+      })
+    );
+  };
+  return (
+    <div className="App">
+      <div className="header">
+        <div className="inputContainer">
+          <input
+            type="text"
+            placeholder="Task.."
+            name="task"
+            value={task}
+            onChange={handleChange}
+          />
+          <input
+            type="number"
+            placeholder="Deadline (in Days).."
+            name="deadline"
+            value={deadLine}
+            onChange={handleChange}
+          />
+        </div>
+        <button onClick={addTask}>Add Task</button>
+      </div>
+      <div className="todoList">
+        {todoList.map((task: ITask, key: number) => {
+          return <TodoTask key={key} task={task} completeTask={completeTask} />;
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default App;
